362a15796c8b53860ea95eca411fa4399dd886d8
Ali Samadi <alisamadi.096@gmail.com>
2025-03-06 22:01:09 -0600

    component written on the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,51 @@
+import { useState } from "react";
+import { episodeList } from "./data";
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to learn more.</p>
+        </section>
+      );
+    }
+    return (
+      <section className="details">
+        <h1>Episode {selectedEpisode.id}</h1>
+        <h2>{selectedEpisode.title}</h2>
+        <p>{selectedEpisode.description}</p>
+        <button>watch now</button>
+      </section>
+    );
+  }
+  function Episodes() {
+    return (
+      <section className="episodes">
+        <h1>Episodes</h1>
+        <ul className="episodes">
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <main>
+        <Episodes />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
